92ba2f97dab3a1332e720147dae5b73ec60c31a4
Antonio <jantonioperez24@gmail.com>
2020-10-07 01:15:23 -0500

    doct

---
+++ docs/precache-manifest.f7b2156c94ce23c53243c44ea0916fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b093484aab6d53afcfd6cfd1ef6864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06f09ea79bd176178327",
+    "url": "/static/css/main.3a37a71f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f09ea79bd176178327",
+    "url": "/static/js/main.a19ec682.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
